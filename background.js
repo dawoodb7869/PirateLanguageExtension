c6fec72bd7dc1e35de70c5a7fe1f21df76f85ad6
Muhammad Dawood <dawoodb7869@gmail.om>
2024-06-28 12:55:24 +0500

    updating icons

--- background.js
+++ background.js
@@ -4,13 +4,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   isActive = !isActive;
   chrome.browserAction.setIcon({
     path: isActive ? {
-      "16": "icon-active-16.png",
-      "48": "icon-active-48.png",
-      "128": "icon-active-128.png"
+      "16": "images/icon-active-16.png",
+      "48": "images/icon-active-48.png",
+      "128": "images/icon-active-128.png"
     } : {
-      "16": "icon-inactive-16.png",
-      "48": "icon-inactive-48.png",
-      "128": "icon-inactive-128.png"
+      "16": "images/icon-inactive-16.png",
+      "48": "images/icon-inactive-48.png",
+      "128": "images/icon-inactive-128.png"
     }
   });
   
--- content.js
+++ content.js
@@ -1,28 +1,28 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.active) {
-      replaceTextWithPirate();
-    }
-  });
-  
-  function replaceTextWithPirate() {
-    fetch(chrome.runtime.getURL('dictionary.json'))
-      .then(response => response.json())
-      .then(dictionary => {
-        document.body.querySelectorAll('*').forEach(element => {
-          element.childNodes.forEach(node => {
-            if (node.nodeType === Node.TEXT_NODE) {
-              let originalText = node.textContent.trim();
-              let words = originalText.split(' ');
-              words.forEach((word, index) => {
-                if (dictionary[word.toLowerCase()]) {
-                  words[index] = dictionary[word.toLowerCase()];
-                }
-              });
-              node.textContent = words.join(' ');
-            }
-          });
-        });
-      })
-      .catch(error => console.error('Error fetching or parsing dictionary.json:', error));
+  if (message.active) {
+    replaceTextWithPirate();
   }
-  
+});
+
+function replaceTextWithPirate() {
+  fetch(chrome.runtime.getURL('dictionary.json'))
+    .then(response => response.json())
+    .then(dictionary => {
+      document.body.querySelectorAll('*').forEach(element => {
+        Array.from(element.childNodes).forEach(node => {
+          if (node.nodeType === Node.TEXT_NODE) {
+            let originalText = node.textContent.trim();
+            let words = originalText.split(' ');
+            words.forEach((word, index) => {
+              let lowerCaseWord = word.toLowerCase();
+              if (dictionary[lowerCaseWord]) {
+                words[index] = dictionary[lowerCaseWord];
+              }
+            });
+            node.textContent = words.join(' ');
+          }
+        });
+      });
+    })
+    .catch(error => console.error('Error fetching or parsing dictionary.json:', error));
+}
